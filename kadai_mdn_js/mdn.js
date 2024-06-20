5fe207461cb0b73181ea9c8543b016d48817cd6c
KihoGangaji <166824748+KihoGangaji@users.noreply.github.com>
2024-06-20 22:12:59 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,13 +1,8 @@
 const date = new Date();
 
-const options = {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric'
-};
 
 const year = date.getFullYear();
-const month = date.getMonth();
+const month = date.getMonth() + 1;
 const day = date.getDate();
 
 console.log(year+'年' + month + '月' + day + '日');

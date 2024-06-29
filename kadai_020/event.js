e575d729cf23906da3758dba92b8719efb6392e8
KihoGangaji <166824748+KihoGangaji@users.noreply.github.com>
2024-06-29 14:00:46 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -6,13 +6,5 @@ const parentList = document.getElementById('text');
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click',() => {
   
-  // li要素を新しく作成する
-  const childList = document.createElement('text');
-  childList.textContent = 'ボタンをクリックしました';
-
-  // parentListの最初の子要素を削除する
-  parentList.removeChild(parentList.firstChild);
-
-  // childListを追加する
-  parentList.appendChild(childList);
+  parentList.textContent = 'ボタンをクリックしました';
 });
